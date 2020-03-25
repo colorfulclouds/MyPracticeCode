9ff9a58e1a63192aedaeb102c9799bce6eb50e51
yuehehe <yuehehe@cechealth.cn>
2020-03-26 01:00:56 +0800

    test

--- src/com/hehe/ArrayAndList/test.java
+++ src/com/hehe/ArrayAndList/test.java
@@ -1,4 +1,9 @@
 package com.hehe.ArrayAndList;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }

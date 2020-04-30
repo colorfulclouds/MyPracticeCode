512455c6d474f76f8d844ab69a0be5c0d38728bb
yuehehe <yuehehe@cechealth.cn>
2020-04-30 20:35:09 +0800

    ArrayListRemove 迭代器删除

--- src/com/hehe/ArrayAndList/ArrayListRemove.java
+++ src/com/hehe/ArrayAndList/ArrayListRemove.java
@@ -1,6 +1,7 @@
 package com.hehe.ArrayAndList;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * ArrayList循环遍历删除 因为其删除的方法里面使用的是删除后 将后续元素往前复制，故对于连续的两个元素，删除第一个，
@@ -22,9 +23,15 @@ public class ArrayListRemove {
         }
         System.out.println();
 
-        remove(list);
+//        remove(list);
+        remove2(list);
     }
 
+
+    /**
+     * 倒序遍历删除 ，可以删除所有为 b 的元素
+     * @param list
+     */
     public static void remove(ArrayList<String> list) {
 
 //        for (String s : list) {  //并发修改异常 Exception in thread "main" java.util.ConcurrentModificationException
@@ -33,13 +40,30 @@ public class ArrayListRemove {
 //            }
 //        }
 
-        for (int i = list.size()-1; i > 0; i--) { //可以删除所有为 b 的元素
+        for (int i = list.size()-1; i > 0; i--) {
             if(list.get(i).equals("b")){
                 list.remove("b");
             }
         }
 
         //remove and print
+        for (String s : list) {
+            System.out.println("element : " + s);
+        }
+
+    }
+
+
+    /**
+     * 迭代方法删除指定元素
+     * @param list
+     */
+    public static void remove2(ArrayList<String> list){
+      for(Iterator<String> ite = list.iterator();ite.hasNext();){
+          if(ite.next().equals("b"))
+              ite.remove();
+      }
+
         for (String s : list) {
             System.out.println("element : " + s);
         }

291f825db0129e420895b58f3ef8dcb8be232d28
yuehehe <yuehehe@cechealth.cn>
2020-04-30 11:55:36 +0800

    注解和继承方式的测试类

--- src/com/hehe/Comparable/TestCompare.java
+++ src/com/hehe/Comparable/TestCompare.java
@@ -4,8 +4,26 @@ import java.util.Collections;
 import java.util.List;
 import org.junit.Test;
 import junit.framework.TestCase;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-public class TestCompare extends TestCase{
+
+/**
+ * @Test注解与TestCase的区别
+ *
+ * 继承TestCase：Junit3的方式，方法名以test为前缀
+ *
+ * @Test注解：Junit4的方式，方法上使用注解@Test
+ *
+ * p.s. :Junit4为了兼容性，jar包里打包的有Junit3的代码
+ *
+ * 如果还想使用Junit4的能力，依然继承TestCase类的时候
+ * （可能如文中说的，可能继承它的一些断言方法）或者是其它外力原因不可更改，可以在测试类上加上注解@RunWith(JUnit4.class)
+ * 显式指定使用Junit4
+ */
+
+//@RunWith(JUnit4.class)
+public class TestCompare {  //extends TestCase
     @Test
     public void test1(){
         List<PersonComparable> list = new ArrayList<PersonComparable>();

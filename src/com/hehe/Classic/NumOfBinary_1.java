ff046de7c415344133a964cfcc255a003a5992f2
yuehehe521 <yuehehe.cs@foxmail.com>
2020-05-06 14:24:21 +0800

    NumOfBinary_1

--- src/com/hehe/Classic/NumOfBinary_1.java
+++ src/com/hehe/Classic/NumOfBinary_1.java
@@ -1,11 +1,22 @@
 package com.hehe.Classic;
 
 
+import java.util.Scanner;
+
 /**二进制中1的个数
  *
  */
-
 public class NumOfBinary_1 {
+
+        public static void main(String[] args) {
+//            System.out.println(nums1(11));
+//            System.out.println(nums2(11));
+
+            Scanner sc = new Scanner(System.in);
+            int input = Integer.parseInt(sc.nextLine());
+            System.out.println(nums2(input));
+        }
+
 
     /*有几个1 就循环几次
      * */
@@ -29,13 +40,6 @@ public class NumOfBinary_1 {
             flag = flag<<1;
         }
         return count;
-    }
-
-
-    public static void main(String[] args) {
-        System.out.println(nums1(11));
-        System.out.println(nums2(11));
-
     }
 
 }

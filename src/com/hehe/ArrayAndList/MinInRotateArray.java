0db0dadcb0253dff7ba988147ac981e0aca272de
yuehehe521 <yuehehe.cs@foxmail.com>
2020-06-03 22:05:16 +0800

    none MinInRotateArray（change）

--- src/com/hehe/ArrayAndList/MinInRotateArray.java
+++ src/com/hehe/ArrayAndList/MinInRotateArray.java
@@ -8,11 +8,18 @@ package com.hehe.ArrayAndList;
 
 public class MinInRotateArray {
     public static void main(String[] args) {
-        int[] arr = {3,4,5,1,2};
+//        int[] arr = {3,4,5,0,1,2};
+        int[] arr = {1,1,1,1,0,1,1}; //此种情况只能顺序查找，见注1
+
         System.out.println(minInRotateArray(arr));
 
     }
 
+    /**
+     * 旋转数组的最小值
+     * @param arr
+     * @return
+     */
     public static int minInRotateArray(int[] arr) {
         if (arr == null || arr.length <= 0)
             return -1;
@@ -25,12 +32,31 @@ public class MinInRotateArray {
                 break;
             }
             mid = (p1+p2)/2;
+            if(arr[p1] == arr[p2] && arr[p2] == arr[mid])   //注 1
+                return  inOrderFindMin(arr,p1,p2);
+
             if(arr[p1]<arr[mid])
                 p1 = mid;
             else if(arr[p2] > arr[mid])
                 p2 = mid;
         }
         return arr[mid];
+    }
+
+    /**
+     * 顺序查找最小值
+     * @param arr
+     * @param p1
+     * @param p2
+     * @return
+     */
+    private static int inOrderFindMin(int[] arr, int p1, int p2) {
+        int min = arr[p1];
+        for (int i = p1+1; i <p2 ; i++) {
+            if(arr[i]<min)
+                min = arr[i] ;
+        }
+        return min;
     }
 
 

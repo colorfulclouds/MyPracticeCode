7e8ad16c34057e4d1011fbcdbd677daad0761291
yuehehe521 <yuehehe.cs@foxmail.com>
2020-06-11 16:02:24 +0800

    FindKthToTail：链表倒数第K个节点

---
+++ src/com/hehe/LinkedList/FindKthToTail.java
@@ -0,0 +1,36 @@
+package com.hehe.LinkedList;
+
+
+class ListNode01 {
+    int val;
+    ListNode01 next;
+
+    ListNode01(int x) {
+        val = x;
+    }
+}
+
+/**
+ * 链表倒数第K个节点
+ */
+public class FindKthToTail {
+    static ListNode01 find(ListNode01 head,int k){
+        if(head == null || k == 0){
+            return null;
+        }
+        ListNode01 p1 = head;
+        for (int i = 0; i < k-1; i++) {
+            p1 = p1.next;
+            if(p1 == null)
+                return null;
+        }
+        ListNode01 p2 = head;
+        while(p1.next !=null){
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p2;
+
+    }
+
+}

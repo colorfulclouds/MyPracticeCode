843a48a0b6d1ed97e01d4756b77d7ef4d44e034f
yuehehe521 <yuehehe.cs@foxmail.com>
2020-06-12 16:52:12 +0800

    数据流中，从右往左数比特块 （本题固定为5：101） 101的个数

---
+++ src/com/hehe/Classic/hw01_Count_101.java
@@ -0,0 +1,31 @@
+package com.hehe.Classic;
+
+import java.util.Scanner;
+
+/**
+ * 在数据流中 从右往左数比特块 （本题固定为5：101） 101的个数
+ */
+public class hw01_Count_101 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int flag = 7; //111
+        int count = 0;
+        int index = 0;
+        boolean isFind = false;
+        while (input != 0) {
+            if ((input & flag) == 5) {
+                count++;
+                isFind = true;
+            } else if(!isFind)   //
+                index++;
+            input >>>= 1;
+        }
+        if (!isFind)
+            index = -1;
+        System.out.println(count + " " + index);
+
+
+    }
+}

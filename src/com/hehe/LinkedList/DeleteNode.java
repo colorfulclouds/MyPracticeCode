672f027078044abeedc4c3cae7fc7128d00506c7
yuehehe521 <yuehehe.cs@foxmail.com>
2020-06-12 16:51:20 +0800

    DeleteNode中添加了reverseList、FindKthToTail变种为找链表中环的入口

--- src/com/hehe/LinkedList/DeleteNode.java
+++ src/com/hehe/LinkedList/DeleteNode.java
@@ -19,20 +19,45 @@ class ListNode {
  * 若使用 C 或 C++ 语言，你不需要 free 或 delete 被删除的节点
  */
 public class DeleteNode {
+
     public static void main(String[] args) {
+
         ListNode head = new ListNode(4);
         head.next = new ListNode(5);
         head.next.next = new ListNode(1);
         head.next.next.next = new ListNode(9);
-
+        //删除链表 测试用例
         ListNode node = deleteNode(head.next, 4);
-
         while (node != null) {
             System.out.println(node.val);
             node = node.next;
         }
+
+        //反转链表 测试用例
+        ListNode reverseHead = reverseList(head);
+        ListNode reverseHead01 = reverseList(null);
+        ListNode reverseHead02 = reverseList(new ListNode(2));
+        while(reverseHead != null){
+            System.out.print(reverseHead.val + " ");
+            reverseHead = reverseHead.next;
+        }
+        while(reverseHead01 != null){
+            System.out.print(reverseHead01.val + " ");
+            reverseHead01 = reverseHead01.next;
+        }
+        while(reverseHead02 != null){
+            System.out.print(reverseHead02.val + " ");
+            reverseHead02 = reverseHead02.next;
+        }
+
     }
 
+    /**
+     * 删除链表中指定值的节点
+     * @param head
+     * @param val
+     * @return
+     */
     static ListNode deleteNode(ListNode head, int val) {
         if (head == null) //1 、头结点为空
             return null;
@@ -40,7 +65,7 @@ public class DeleteNode {
         ListNode p1 = head;
         ListNode p2 = p1.next;
 
-        if (p1.val == val) {// 2、第一个结点为要删除的节点
+        if (p1.val == val) {// 2、第一个结点为要删除的节点（包含只有一个结点的情况）
             return head.next;
         }
 
@@ -54,5 +79,31 @@ public class DeleteNode {
             return head;
         } else
             return head;
+    }
+
+    /**
+     * 反转链表
+     * @param head
+     * @return
+     */
+    static ListNode reverseList(ListNode head){
+        if(head == null)
+            return null;
+        if(head.next == null)
+            return head;
+
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode tmp = cur.next;
+
+        while(tmp!=null){
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+            tmp = tmp.next;
+        }
+        cur.next = pre;
+        return cur;
+
     }
 }

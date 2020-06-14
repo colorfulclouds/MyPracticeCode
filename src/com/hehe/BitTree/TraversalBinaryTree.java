c0f1261c8a0bf04f9672696b8d3ce532cddc7084
yuehehe521 <yuehehe.cs@foxmail.com>
2020-06-14 18:08:51 +0800

    层序遍历及层序遍历的变种（每层一行打印、之字形打印）

--- src/com/hehe/BitTree/TraversalBinaryTree.java
+++ src/com/hehe/BitTree/TraversalBinaryTree.java
@@ -1,23 +1,138 @@
 package com.hehe.BitTree;
 
-import java.util.Stack;
+import javax.jnlp.IntegrationService;
+import java.util.*;
 
-/**二叉树遍历：递归和非递归版本的代码
- *
+/**
+ * 二叉树遍历：递归和非递归版本的代码
  */
 
 public class TraversalBinaryTree {
 
     // Node 结构体
-    public static class Node{
+    public static class Node {
         public int value;
         public Node left;
         public Node right;
 
-        public Node (int data){
+        public Node(int data) {
             this.value = data;
         }
     }
+
+    //=========================层序遍历================================
+
+    /**
+     * 层序遍历 用队列
+     * @param head
+     */
+    public static void levelOrder(Node head) {
+        Queue<Node> queue = new ArrayDeque<>(); //new LinkedList<>();
+//        queue.
+
+        if (head != null)
+            queue.add(head);
+        while (!queue.isEmpty()) {  //注意栈空的判断 不能用queue != null 判断
+            Node peek = queue.poll();
+            System.out.print(peek.value + " ");
+            if(peek.left!=null)
+                queue.add(peek.left);
+            if(peek.right!=null)
+                queue.add(peek.right);
+        }
+    }
+
+    /**
+     * 层序遍历 用队列
+     * 每一层打印一行（本方法返回数组，每个元素时每一层的结点）
+     * @param head
+     * @return
+     */
+    public static List<List<Integer>> levelOrder01(Node head) {
+        Queue<Node> queue = new ArrayDeque<>(); //new LinkedList<>();
+        List<List<Integer>> list = new ArrayList<>();
+        if (head != null)
+            queue.add(head);
+        while (!queue.isEmpty()) {  //注意栈空的判断 不能用queue != null 判断
+            int size = queue.size();  //当前层的结点数目
+            ArrayList<Integer> tmp = new ArrayList<>();  //非双端队列 数组列表即可
+            for (int i = 0; i < size; i++) { //遍历一层的结点 并将下一层全部入队列
+                Node peek = queue.poll();
+                tmp.add(peek.value);
+                if(peek.left!=null)
+                    queue.add(peek.left);
+                if(peek.right!=null)
+                    queue.add(peek.right);
+            }
+            list.add(tmp);
+        }
+        return list;
+    }
+
+    /**
+     * 之字形打印树
+     * @param head
+     * @return
+     *
+     * hehe
+     */
+    public static List<List<Integer>> levelOrder02(Node head) {
+        Queue<Node> queue = new ArrayDeque<>(); //new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if (head != null)
+            queue.add(head);
+        int n = 0;  //判断奇偶层标志变量
+        while (!queue.isEmpty()) {  //注意栈空的判断 不能用queue != null 判断
+            int size = queue.size();  //当前层的结点数目
+            LinkedList<Integer> tmp = new LinkedList<>();  //双端队列
+
+            for (int i = 0; i < size; i++) { //遍历一层的结点 并将下一层全部入队列
+                Node peek = queue.poll();
+                if(n==0)  //也可以用结果数组的size ：if(res.size % 2 == 0)
+                    tmp.addLast(peek.value);
+                else
+                    tmp.addFirst(peek.value);
+                if(peek.left!=null)
+                    queue.add(peek.left);
+                if(peek.right!=null)
+                    queue.add(peek.right);
+            }
+            res.add(tmp);
+            n = (n == 0)? 1:0;
+
+        }
+        return res;
+    }
+
+
+    /**
+     * 之字形打印树
+     * @param head
+     * @return
+     *
+     *网友答案
+     */
+    public static List<List<Integer>> levelOrder03(Node head) {
+        Queue<Node> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(head != null)
+            queue.add(head);
+        while(!queue.isEmpty()) {
+            LinkedList<Integer> tmp = new LinkedList<>();
+            for(int i = queue.size(); i > 0; i--) {
+                Node node = queue.poll();
+                if(res.size() % 2 == 0) tmp.addLast(node.value); // 偶数层 -> 队列头部
+                else tmp.addFirst(node.value); // 奇数层 -> 队列尾部
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+
+
+
     //=========================前 中 后序的 递归实现================================
     public static void preOrderRecur(Node head) {
         if (head != null) {
@@ -52,6 +167,7 @@ public class TraversalBinaryTree {
     /**
      * 先序
      * ！！！！！！！
+     *
      * @param head
      */
     public static void preOrderUnRecur(Node head) {
@@ -75,20 +191,21 @@ public class TraversalBinaryTree {
 
     /**
      * 中序
+     *
      * @param head
      */
     public static void inOrderUnRecur(Node head) {
         System.out.println("in-order: ");
-        if(head!=null){
+        if (head != null) {
             Stack<Node> stack = new Stack<Node>();
 
-            while(!stack.isEmpty() || head!=null){
-                if(head!=null){
+            while (!stack.isEmpty() || head != null) {
+                if (head != null) {
                     stack.push(head);
-                    head=head.left;
-                }else{
+                    head = head.left;
+                } else {
                     head = stack.pop();
-                    System.out.print(head.value+" ");
+                    System.out.print(head.value + " ");
                     head = head.right;
                 }
             }
@@ -99,8 +216,8 @@ public class TraversalBinaryTree {
     /**
      * 后序
      * 思想：前序是根左右，后序是左右根，借鉴前序，根节点先暂存起来，左右完毕之后再打印根节点
-     *       需要双栈，另一个栈暂存根节点
-     *       ！！最后需要反序一下！！  根右左---》左右根
+     * 需要双栈，另一个栈暂存根节点
+     * ！！最后需要反序一下！！  根右左---》左右根
      *
      * @param head
      */
@@ -121,13 +238,13 @@ public class TraversalBinaryTree {
                     s1.push(head.right);
                 }
                 while (!s2.isEmpty()) {
-                      s3.push(s2.pop());
+                    s3.push(s2.pop());
 //                    System.out.print(s2.pop().value + " ");
                 }
             }
         }
         //逆序输出才是正确答案
-        while(!s3.isEmpty()){
+        while (!s3.isEmpty()) {
             System.out.print(s3.pop().value + " ");
         }
         System.out.println();
@@ -135,6 +252,7 @@ public class TraversalBinaryTree {
 
     /**
      * can not understand 后序遍历 炫技版
+     *
      * @param h
      */
     public static void posOrderUnRecur02(Node h) {
@@ -190,10 +308,19 @@ public class TraversalBinaryTree {
 //        preOrderUnRecur(head);
 
 //		inOrderUnRecur(head);
-		posOrderUnRecur01(head);
+        posOrderUnRecur01(head);
 //        1 2 4 3 6 7 9 11 10 8 5
 
 //		posOrderUnRecur02(head);
+
+        System.out.println("=====================层序遍历================");
+//        levelOrder(head);
+        List<List<Integer>> list = levelOrder01(head);
+        System.out.println(list.toString());
+        List<List<Integer>> list1 = levelOrder02(head);
+        System.out.println(list1.toString());
+        List<List<Integer>> list2 = levelOrder03(head);
+        System.out.println(list2.toString());
     }
 
 }

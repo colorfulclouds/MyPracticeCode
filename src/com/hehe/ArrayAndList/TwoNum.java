e2ddf4ed630fccff492474f15f64a981d6c169a3
yuehehe521 <yuehehe.cs@foxmail.com>
2020-07-18 19:17:13 +0800

    twoSumInOrdered(排序数组两个数等于target值)、sumTos(和为 s 的连续正数序列)、zf_me

--- src/com/hehe/ArrayAndList/TwoNum.java
+++ src/com/hehe/ArrayAndList/TwoNum.java
@@ -1,6 +1,8 @@
 package com.hehe.ArrayAndList;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /*
@@ -9,6 +11,8 @@ import java.util.Map;
 public class TwoNum {
 
     public static void main(String[] args) {
+
+        //1、任意数组中的两个数等于既定的target值：暴力O（n^2）
         int[] nums = {3, 9, 8, 1};
         int target = 9;
         int[] rst = twoSum(nums, target);
@@ -16,16 +20,97 @@ public class TwoNum {
 //			System.out.print(i);
 //		}
         System.out.print("[" + rst[0] + "," + rst[1] + "]");
+        System.out.println();
+
+        //2、排序数组中的两个数等于既定的target值 :双指针 O（n）
+        int[] nums1 = {1, 2, 4, 7, 11, 15};
+        int[] res = twoSumInOrdered(nums1, 15);
+        for (int i : res)
+            System.out.print(i + " ");
+
+        //3、和为 s 的连续正数序列
+        System.out.println("=====");
+        List<int[]> res1 = sumTos(9);
+        for (int[] i : res1) {
+                System.out.print(i[0] + "~" +i[1]);
+            System.out.println();
+        }
+
+
     }
 
-    //两数之和等于target，并输出两数下标（数组类型）
+    /**
+     * 3、和为 s 的连续正数序列
+     * @param s
+     * @return
+     */
+    public static List<int[]> sumTos(int s) {
+        int i = 1, j = 2;
+        List<int[]> list = new ArrayList<>();
+
+        while (i < (1 + s) / 2) {
+            if (Sum(i, j) == s){
+                list.add(new int[]{i,j});
+                j++;
+            }
+            else if (Sum(i, j) < s && i < ((1 + s) / 2)) {
+                j++;
+            } else if ((Sum(i, j) > s && i < ((1 + s) / 2)))
+                i++;
+        }
+        return list;
+    }
+
+    private static int Sum(int i, int j) {
+        return (j - i + 1) * i + (j - i + 1) * (j - i) / 2;
+    }
+
+
+    /**
+     *
+     * @param s
+     * @return
+     */
+    public static List<int[]> sumTos01(int s) {
+
+        return null;
+    }
+
+    /**
+     * 2、求排序数组中的两个数等于既定的target值
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSumInOrdered(int[] nums, int target) {
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            if (nums[i] + nums[j] == target)
+                return new int[]{nums[i], nums[j]};
+            else if (nums[i] + nums[j] > target)
+                j--;
+            else
+                i++;
+        }
+        return null;
+
+    }
+
+    /**
+     * 1、无序数组中两数之和等于target，并输出两数下标（数组类型）
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
     public static int[] twoSum(int[] nums, int target) {
 //        int[] res = new int[2];
 
-        for (int i = 0; i <nums.length ; i++) {
+        for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target)
-                    return new int[]{i,j};
+                if (nums[i] + nums[j] == target)
+                    return new int[]{i, j};
             }
         }
         return null;
@@ -50,5 +135,7 @@ public class TwoNum {
 ////		    throw new IllegalArgumentException("No two sum solution");
 //        return null;
     }
+
+
 }
 
--- src/test/zf_me0716test.java
+++ src/test/zf_me0716test.java
@@ -24,7 +24,7 @@ public class zf_me0716test {
 
         //看源码了解到，如果使用封装类Equals判断两个变量是否相等，首先需要他们是同一个类型的(同是Double或者同是Integer)，如果是值相等，就是true
         //用 == 判断时，判断的是地址，如果有表达式参与，则比较的是值
-        //Integer i= XXX 自动装箱，和Integer i= Integer.valueOf(10) 等价，范围在（-128~127）可以使用Integer缓冲池，超过此范围的自动装箱的两个数是不等的
+        //Integer i= XXX 自动装箱，和Integer i= Integer.valueOf(10) 等价，范围在（-128~127）可以使用Integer缓冲池，超过此范围的自动装箱的两个数用==判断是不等的，因为超过此范围的会重新new，地址不一样了
         //Integer i= XXX 和 Integer.valueOf(XXX)区别是 new 每次都会new一个新的对象，而Integer.valueOf(XXX) 可能会用缓存池的对象
 
 

11808ae19cac201acf2c848db27886afef9cd6eb
yuehehe521 <yuehehe.cs@foxmail.com>
2020-08-18 15:13:09 +0800

    none(串)

--- src/com/hehe/RecursiveAndDynamic/IsSubSequence.java
+++ src/com/hehe/RecursiveAndDynamic/IsSubSequence.java
@@ -1,4 +1,4 @@
-package com.hehe.String;
+package com.hehe.RecursiveAndDynamic;
 
 
 /**
@@ -21,15 +21,15 @@ public class IsSubSequence {
     public static void main(String[] args) {
         String s = "abcde";
         String t = "ace";
-        System.out.println(isSubsequence(s,t)); //true
+        System.out.println(isSubsequence(s, t)); //true
 
         String s1 = "abcde";
         String t1 = "cf";
-        System.out.println(isSubsequence(s1,t1)); //false
+        System.out.println(isSubsequence(s1, t1)); //false
         System.out.println("++++++++++");
 
-        System.out.println(isSubsequence01(s,t)); //false
-        System.out.println(isSubsequence01(s1,t1)); //false
+        System.out.println(isSubsequence01(s, t)); //false
+        System.out.println(isSubsequence01(s1, t1)); //false
 
     }
 
@@ -42,15 +42,15 @@ public class IsSubSequence {
      * @return
      */
     public static boolean isSubsequence(String s, String t) {
-        boolean[][] table = new boolean[t.length() +1][s.length()+1];
+        boolean[][] table = new boolean[t.length() + 1][s.length() + 1];
 
         for (int col = 0; col <= s.length(); col++) {
             table[0][col] = true;
         }
         for (int row = 1; row <= t.length(); row++) {
             for (int col = 1; col <= s.length(); col++) {
-                char chr1 = t.charAt(row-1);
-                char chr2 = s.charAt(col-1);
+                char chr1 = t.charAt(row - 1);
+                char chr2 = s.charAt(col - 1);
                 //自己的办法总是很笨 -_-!
 //                if(chr1 == chr2){
 //                    table[row][col] = table[row - 1][col - 1];
@@ -62,7 +62,6 @@ public class IsSubSequence {
 //                }
 //                else
 //                    table[row][col] = false;
-
                 if (chr1 == chr2)
                     table[row][col] = table[row - 1][col - 1];
                 else
@@ -85,15 +84,15 @@ public class IsSubSequence {
     public static boolean isSubsequence01(String s, String t) {
         String tmp = "";// = new String();
         int i = 0;
-       return isSubCore(s,i,tmp,t);
+        return isSubCore(s, i, tmp, t);
 
     }
 
-    private static boolean isSubCore(String s, int i, String tmp,String t) {
-        if(tmp.equals(t))
+    private static boolean isSubCore(String s, int i, String tmp, String t) {
+        if (tmp.equals(t))
             return true;
-        if(i<s.length())
-            return isSubCore(s,i+1,tmp,t) || isSubCore(s,i+1,tmp + String.valueOf(s.charAt(i)),t);
+        if (i < s.length())
+            return isSubCore(s, i + 1, tmp, t) || isSubCore(s, i + 1, tmp + s.charAt(i), t);
         else
             return false;
 
--- src/com/hehe/String/SubStringQs.java
+++ src/com/hehe/String/SubStringQs.java
@@ -4,20 +4,23 @@ public class SubStringQs {
 
     public static void main(String[] args) {
         String s = "aabbaabaa";
-        int m = 2;
-//        int res = characterReplacement(s, m);
-        int res = characterReplacement02(s, 2);
-        int res1 = characterReplacement02(s, 1);
+
+
+        int res = characterReplacement(s, 2);
+        int res1 = characterReplacement(s, 1);
         System.out.println(res);
         System.out.println(res1);
     }
+
+
     /**
      * 最长完美字串
+     * 滑动窗口
      * @param s
      * @param k
      * @return
      */
-    public static int characterReplacement02(String s, int k) {
+    public static int characterReplacement(String s, int k) {
         int[] map = new int[26];
         if (s == null) {
             return 0;
@@ -26,15 +29,22 @@ public class SubStringQs {
         int left = 0;
         int right = 0;
         int historyCharMax = 0;
-        for (right = 0; right < chars.length; right++) {
+
+//        int res = 0; //可以定义结果变量 方便理解
+
+        for (right = 0; right < chars.length; right++) { //窗口右侧一直往右移
             int index = chars[right] - 'a';
             map[index]++;
             historyCharMax = Math.max(historyCharMax, map[index]);
-            if (right - left + 1 > historyCharMax + k) {
+            if (right - left + 1 > historyCharMax + k) { //窗口内替换后仍无法达到历史最大完美子串长度 故窗口左侧边界收缩
                 map[chars[left] - 'a']--;
                 left++;
             }
+//            else
+//                res = Math.max(right - left + 1, res);
+
         }
         return chars.length - left;
+//        return res;
     }
 }

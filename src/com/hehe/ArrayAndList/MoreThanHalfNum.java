9d78be41901a20b633b3410ff812f788e981b35d
yuehehe521 <yuehehe.cs@foxmail.com>
2020-09-01 20:53:55 +0800

    MoreThanHalfNum01(57%)

--- src/com/hehe/ArrayAndList/MoreThanHalfNum.java
+++ src/com/hehe/ArrayAndList/MoreThanHalfNum.java
@@ -1,9 +1,17 @@
 package com.hehe.ArrayAndList;
 
+import java.util.Arrays;
+
 public class MoreThanHalfNum {
     public static void main(String[] args) {
         int[] a = {1, 2, 2, 2, 3, 4, 2};
         System.out.println(MoreThanHalfNum((a)));
+
+
+        System.out.println("=================");
+        Arrays.sort(a);
+        int mul = a[a.length / 2];
+        System.out.println(mul);
     }
 
 
@@ -27,4 +35,68 @@ public class MoreThanHalfNum {
         }
         return res; //最后一次将 times 置为 1 的那个数，因为要找的那个数字比其他数字数量之和还要多
     }
+
+
+    /**
+     * 不存在返回 0
+     *
+     * 通过率 57%
+     * @param a
+     * @return
+     */
+    public static int MoreThanHalfNum01(int[] a) {
+        int res = 0;
+        if(a !=null && a.length > 0){
+            Arrays.sort(a);
+            int mid = a[a.length/2];
+            int first = getFirstK(a,0,a.length/2,mid);
+            int last = getLastK(a,a.length/2,a.length,mid);
+            if((last - first + 1) > a.length/2){
+                res = 2;
+            }else
+                res = 0;
+        }
+
+        return res;
+    }
+
+    public static int getFirstK(int[] nums, int start, int end, int k) {
+        if (start > end)
+            return -1;
+
+        int mid = (start + end) >> 1;
+
+        if (nums[mid] == k) {
+            if ((nums[mid - 1] != k && mid > 0) || mid == 0)
+                return mid;
+            else
+                end = mid - 1;
+        } else if (nums[mid] > k)
+            end = mid - 1;
+        else
+            start = mid + 1;
+
+        return getFirstK(nums, start, end, k);
+    }
+    public static int getLastK(int[] nums, int start, int end, int k) {
+        if (start > end)
+            return -1;
+
+        int mid = (start + end) >> 1;
+
+        if (nums[mid] == k) {
+            if ((nums[mid + 1] != k && mid < nums.length) || mid == nums.length)
+                return mid;
+            else
+                start = mid + 1;
+        } else if (nums[mid] > k)
+            end = mid - 1;
+        else
+            start = mid + 1;
+
+        return getLastK(nums, start, end, k);
+    }
+
+
+
 }

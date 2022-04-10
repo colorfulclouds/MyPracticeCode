5aa71e8167c2cdbb1f93d7d6e0caffd4c0893b44
yuehehe521 <yuehehe.cs@foxmail.com>
2022-04-10 19:02:49 +0800

    2022.4.9-Bitree

--- src/com/hehe/BiTree/BiTree_Feature.java
+++ src/com/hehe/BiTree/BiTree_Feature.java
@@ -3,7 +3,8 @@ package com.hehe.BiTree;
 public class BiTree_Feature {
 
     /**
-     * 最大深度
+     * 1 最大深度
+     *
      * @param root
      * @return
      */
@@ -12,12 +13,13 @@ public class BiTree_Feature {
             return 0;
         int depth_left = max_depth(root.left);
         int depth_right = max_depth(root.right);
-        return depth_left > depth_right ? depth_left + 1 : depth_right + 1;
+//        return depth_left > depth_right ? depth_left + 1 : depth_right + 1;
+        return Math.max(depth_left, depth_right) + 1;
     }
 
 
     /**
-     * 求给定二叉树的最小深度。最小深度是指树的根结点到最近叶子结点的最短路径上结点的数量。
+     * 2 求给定二叉树的最小深度。最小深度是指树的根结点到最近叶子结点的最短路径上结点的数量。
      * <p>
      * 若只有根结点和右结点，则最小深度是根结点到最近的叶子结点 ：右结点的 最短路径上结点的数量
      *
@@ -35,11 +37,12 @@ public class BiTree_Feature {
 
 
     /**
-     * 判断是否是平衡二叉树
+     * 3 判断是否是平衡二叉树
+     *
      * @param root
      * @return
      */
-    public static boolean IsBalance(TreeNode root){
+    public static boolean IsBalance(TreeNode root) {
         return recur(root) != -1;
     }
 
@@ -49,29 +52,30 @@ public class BiTree_Feature {
      * @param root
      * @return
      */
-    public static int recur(TreeNode root){
-        if(root == null)
+    public static int recur(TreeNode root) {
+        if (root == null)
             return 0;
 
         int left = recur(root.left);
-        if(left == -1)
+        if (left == -1)
             return -1;
 
         int right = recur(root.right);
-        if(right == -1)
+        if (right == -1)
             return -1;
 
-        return Math.abs(left - right) < 2 ? Math.max(left,right) + 1 : -1;
+        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
 
     }
 
     /**
-     * 是否是镜像二叉树
+     * 4 是否是镜像二叉树
+     *
      * @param root
      * @return
      */
     public static boolean isSymmetricTree(TreeNode root) {
-        if(root==null)
+        if (root == null)
             return true;
         return Helper(root.left, root.right);
     }
@@ -83,18 +87,19 @@ public class BiTree_Feature {
             return false;
 
         //镜像的判断（互为镜像的两个结点继续判断： A结点 的左子结点 和 B结点的右子结点
-        //                                    A结点 的右子结点 和 B结点的左子结点）
+        //                                 A结点 的右子结点 和 B结点的左子结点）
         return left.val == right.val
                 && Helper(left.left, right.right)
                 && Helper(left.right, right.left);
     }
 
     /**
-     * 输出一颗二叉树的镜像树
+     * 5 求一颗二叉树的镜面反转树
+     *
      * @param root
      */
-    static TreeNode Mirror(TreeNode root){
-        if(root==null)
+    static TreeNode Mirror(TreeNode root) {
+        if (root == null)
             return root;
 //        if(root.left == null && root.right == null)
 //            return;
@@ -102,16 +107,17 @@ public class BiTree_Feature {
         root.left = root.right;
         root.right = tmp;
 
-        if(root.left != null)
+        if (root.left != null)
             Mirror(root.left);
-        if(root.right != null)
+        if (root.right != null)
             Mirror(root.right);
         return root;
     }
 
 
     /**
-     * 叶子节点个数
+     * 6 叶子节点个数
+     *
      * @param head
      * @return
      */
@@ -127,73 +133,82 @@ public class BiTree_Feature {
 
     public static void main(String[] args) {
         System.out.println("========================================最小/最大深度=======================================");
-//        TreeNode head = new TreeNode(1);
-//        head.left = new TreeNode(2);
-//        head.right = new TreeNode(3);
-//        head.right.left = new TreeNode(4);
-//        head.left.left = new TreeNode(2);
-//        head.left.right = new TreeNode(3);
-//        head.left.right.left = new TreeNode(5);
-//        head.left.right.right = new TreeNode(4);
-//
-//        TreeNode root = new TreeNode(1);
-//        root.left = new TreeNode(2);
-//        root.left.left = new TreeNode(4);
-//        root.right = new TreeNode(3);
-
-//        System.out.println("min_depth1: " + min_depth(head));
-//        System.out.println("min_depth2: " + min_depth(root));
-//
-//        System.out.println("max_depth2： " + max_depth(head));
-//        System.out.println("max_depth1： " + max_depth(root));
-
-
-        System.out.println("=====================后序递归判断是否是平衡二叉树=======================");
-//        TreeNode root = new TreeNode(3);
-//        System.out.println(IsBalance(root));
-//
-//        root.left = new TreeNode(3);
-////        root.right = new Node(4);  //注释便不平衡的树
-//        root.left.left = new TreeNode(4);
-//        System.out.println(IsBalance(root));
-
-        System.out.println("===============================是否是镜像二叉树============================");
-//        TreeNode root = new TreeNode(1);
-//        root.left = new TreeNode(2);
-//        root.right = new TreeNode(2);
-//        root.left.left = new TreeNode(3);
-//        root.left.right = new TreeNode(4);
-//        root.right.left = new TreeNode(4);
-//        root.right.right = new TreeNode(3);
-//
-//        System.out.println(isSymmetricTree(root));
-        System.out.println("=============================找镜像二叉树=====================");
+        TreeNode head = new TreeNode(1);
+        head.left = new TreeNode(2);
+        head.right = new TreeNode(3);
+        head.right.left = new TreeNode(4);
+        head.left.left = new TreeNode(2);
+        head.left.right = new TreeNode(3);
+        head.left.right.left = new TreeNode(5);
+        head.left.right.right = new TreeNode(4);
 
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
-        root.right = new TreeNode(2);
-        root.left.left = new TreeNode(3);
-        root.left.right = new TreeNode(4);
-        root.right.left = new TreeNode(4);
-        root.right.right = new TreeNode(3);
-        TreeNode res = Mirror(root);
+        root.left.left = new TreeNode(4);
+        root.left.left.left = new TreeNode(4);
+        root.right = new TreeNode(3);
+
+        System.out.println("min_depth1: " + min_depth(head));
+        System.out.println("min_depth2: " + min_depth(root));
+
+        System.out.println("max_depth2： " + max_depth(head));
+        System.out.println("max_depth1： " + max_depth(root));
+
+
+        System.out.println("=====================后序递归判断是否是平衡二叉树=======================");
+        TreeNode root1 = new TreeNode(3);
+        System.out.println(IsBalance(root1));
+
+        root1.left = new TreeNode(3);
+//        root1.right = new Node(4);  //注释便不平衡的树
+        root1.left.left = new TreeNode(4);
+        System.out.println(IsBalance(root1));
+
+        System.out.println("===============================是否是镜像二叉树============================");
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(2);
+        root2.left.left = new TreeNode(3);
+        root2.left.right = new TreeNode(4);
+        root2.right.left = new TreeNode(4);
+        root2.right.right = new TreeNode(3);
+
+        System.out.println(isSymmetricTree(root2));
+        System.out.println("=============================找镜像二叉树=====================");
+
+        TreeNode root3 = new TreeNode(1);
+        root3.left = new TreeNode(2);
+        root3.right = new TreeNode(2);
+        root3.left.left = new TreeNode(3);
+        root3.left.right = new TreeNode(4);
+        root3.right.left = new TreeNode(4);
+        root3.right.right = new TreeNode(3);
+        TreeNode res = Mirror(root3);
         System.out.println(res.val);
 
 
         System.out.println("=============================叶子节点个数=====================");
-        TreeNode head = new TreeNode(5);
-        head.left = new TreeNode(3);
-        head.right = new TreeNode(8);
-        head.left.left = new TreeNode(2);
-        head.left.right = new TreeNode(4);
-        head.left.left.left = new TreeNode(1);
-        head.right.left = new TreeNode(7);
-        head.right.left.left = new TreeNode(6);
-        head.right.right = new TreeNode(10);
-        head.right.right.left = new TreeNode(9);
-        head.right.right.right = new TreeNode(11);
+        TreeNode head2 = new TreeNode(5);
+        head2.left = new TreeNode(3);
+        head2.right = new TreeNode(8);
+        head2.left.left = new TreeNode(2);
+        head2.left.right = new TreeNode(4);
+        head2.left.left.left = new TreeNode(1);
+        head2.right.left = new TreeNode(7);
+        head2.right.left.left = new TreeNode(6);
+        head2.right.right = new TreeNode(10);
+        head2.right.right.left = new TreeNode(9);
+        head2.right.right.right = new TreeNode(11);
 
-        System.out.println(leafs(head));
+
+        TreeNode root4 = new TreeNode(1);
+        root4.left = new TreeNode(2);
+        root4.left.left = new TreeNode(4);
+        root4.left.left.left = new TreeNode(4);
+        root4.right = new TreeNode(3);
+
+        System.out.println(leafs(head2));
+        System.out.println(leafs(root4));
 
 
     }
--- src/com/hehe/BiTree/FindPath2Sum.java
+++ src/com/hehe/BiTree/FindPath2Sum.java
@@ -1,81 +1,112 @@
 package com.hehe.BiTree;
 
+import com.sun.source.tree.Tree;
+
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
+
 
 /**
  * 二叉树中和为某一值的路径
- *
+ * <p>
  * 路径：从根节点到叶子结点
  * 路段：路径上某一段
  */
 public class FindPath2Sum {
     public static void main(String[] args) {
-        Node head = new Node(10);
-        head.left = new Node(5);
-        head.left.left = new Node(4);
-        head.left.right = new Node(7);
-        head.right = new Node(12);
-        head.right.right = new Node(1);
-//        findPath(head, 22);  //错误的
+//        TreeNode head = new TreeNode(10);
+//        head.left = new TreeNode(5);
+//        head.left.left = new TreeNode(4);
+//        head.left.right = new TreeNode(7);
+//        head.right = new TreeNode(12);
+//        head.right.right = new TreeNode(1);
+        TreeNode head01 = new TreeNode(1);
+        head01.left = new TreeNode(1);
+        head01.right = new TreeNode(2);
+        head01.left.left = new TreeNode(1);
+        head01.left.right = new TreeNode(1);
+//        head.right.left = new TreeNode(1);
+//        head.right.right = new TreeNode(1);
 
-        LinkedList<List<Integer>> res = findPath01(head, 22);
+        List list = new ArrayList();
+        Method(head01, list, 3);
+//        findPath(head, 22);  //错误的
+        System.out.println("+++++++++++++++++++++++++");
+
+        LinkedList<List<Integer>> res = findPath01(head01, 3);
         System.out.println(res.toString());
 
 
         System.out.println("==================");
 
-        ArrayList<ArrayList<Integer>> res01 = FindPath(head,22);
+        ArrayList<ArrayList<Integer>> res01 = FindPath(head01, 3);
         System.out.println(res01.toString());
 
 
     }
 
+    public static void Method(TreeNode node, List<Integer> list, int number) {
+        if(null == node)
+            return;
+        list.add(node.val);
+
+        if (node.left == null && node.right == null) {
+            int sum = 0;
+
+            for (int i = 0; i < list.size(); i++) {
+                sum = sum + list.get(i);
+            }
+            if (sum == number) {
+                System.out.println(list);
+            }
+            return;
+        }
+        Method(node.left, list, number);
+        list.remove(list.size() - 1);
+        
+        Method(node.right, list, number);
+        list.remove(list.size() - 1);
+
+    }
+
+
     static LinkedList<List<Integer>> res = new LinkedList<>();
     static LinkedList<Integer> path = new LinkedList<>();
 
-    public static LinkedList<List<Integer>> findPath01(Node head, int target) {
-        if (head == null) return res;
-        path.add(head.value);
-        target -= head.value;
+    public static LinkedList<List<Integer>> findPath01(TreeNode head, int target) {
+        if (head == null)
+            return res;
+        path.add(head.val);
+        target -= head.val;
         if (target == 0 && head.left == null && head.right == null)
             res.add(new LinkedList<>(path));   //需要new 一个LinkedList结点 将path转型放进去
 
         findPath01(head.left, target);
         findPath01(head.right, target);
         path.removeLast();   // !
+//        list.remove(list.size()-1); //同上
         return res;
     }
 
 
-//==========================================hhh==============================================
+    //==========================================hhh==============================================
     private static ArrayList<ArrayList<Integer>> res01 = new ArrayList<>();
     private static ArrayList<Integer> list = new ArrayList<Integer>();
 
-    public static ArrayList<ArrayList<Integer>> FindPath(Node root,int target) {
-        if(root == null)
+    public static ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+        if (root == null)
             return res01;
-        list.add(root.value);
-        target -= root.value;
-        if(target == 0 && root.left == null && root.right == null)
+        list.add(root.val);
+        target -= root.val;
+        if (target == 0 && root.left == null && root.right == null)
             res01.add(new ArrayList<>(list));  // !!!
-        FindPath(root.left,target);
-        FindPath(root.right,target);
-        list.remove(list.size()-1);
+        FindPath(root.left, target);
+        FindPath(root.right, target);
+        list.remove(list.size() - 1);
         return res01;
     }
 //=============================================hhh===========================================
 
-    // Node 结构体
-    public static class Node {
-        public int value;
-        public Node left;
-        public Node right;
-
-        public Node(int data) {
-            this.value = data;
-        }
-    }
 }
